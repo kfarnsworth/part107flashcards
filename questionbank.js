e56cebb37a3a35ecc16be84ace7500db56f8d489
Kyle Farnsworth <kyle@farnsworthtech.com>
2025-02-08 02:43:19 +0000

    new web site

---
+++ questionbank.js
@@ -0,0 +1,11 @@
+
+qbank = [ 
+    { "question": "Question 1", "answers": [ "Answer1", "Answer2", "Answer3" ] },
+    { "question": "Question 2", "answers": [ "Answer4", "Answer5", "Answer6" ] }
+];
+
+abank = [ 
+    { "correct": "B", "explain":"BLAH BLAH BLAH1" },
+    { "correct": "A", "explain":"BLAH BLAH BLAH2" }
+];
+
